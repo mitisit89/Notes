5de0dfc31fdb31527da17c4666536ca60944fd24
MitiSit <mitisit89@gmail.com>
2022-03-15 13:04:02 +0300

    create models

--- core/notes/models.py
+++ core/notes/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+
+class UserProfile(models.Model):
+    username=models.CharField(max_length=100,db_index=True)
+    user_emai=models.CharField(max_length=100,db_index=True)
+    user_password=models.CharField(max_length=140)
 class Note(models.Model):
     title=models.CharField(max_length=150,db_index=True)
     body=models.TextField(blank=True,db_index=True)
     created=models.DateTimeField(auto_now_add=True)
+    ref_to_user=models.ForeignKey(UserProfile,on_delete=models.CASCADE)
