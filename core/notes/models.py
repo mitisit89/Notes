41ab87938f08f3e37335fdfdf2712f33c57e1cc1
MitiSit <mitisit89@gmail.com>
2022-04-03 19:22:29 +0300

    create slug fn

--- core/notes/models.py
+++ core/notes/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.shortcuts import reverse
+from django.utils.text import slugify
+from time import time
+
+
+def create_slug(s):
+    slug = slugify(s, allow_unicode=True)
+    return f"{slug}-{str(int(time()))}"
+
 
 # Create your models here.
 
@@ -11,8 +20,17 @@ class UserProfile(models.Model):
 
 class Note(models.Model):
     title = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=150, blank=True, unique=True)
     body = models.TextField(blank=True, db_index=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = create_slug(self.title)
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ["-created"]
