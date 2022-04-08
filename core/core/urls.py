3fdb11cb06497152435bbb1a32a74c5dc2c72a14
MitiSit <mitisit89@gmail.com>
2022-04-08 17:23:58 +0300

    add login view

--- core/core/urls.py
+++ core/core/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("notes/", include("notes.urls")),
-    path("auth/", include("authapp.urls")),
+    path("", include("authapp.urls")),
 ]
